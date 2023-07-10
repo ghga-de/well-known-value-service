c918e23f8b2d5be673f5969731285b3bfc7e5054
TheByronHimes <TheByronHimes@gmail.com>
2023-07-10 09:37:45 +0000

    Update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -23,25 +23,13 @@ from tests.fixtures.joint import JointFixture, joint_fixture  # noqa: F401
 
 
 @pytest.mark.asyncio
-async def test_happy_retrieval_simple_value(joint_fixture: JointFixture):  # noqa: F811
+async def test_happy_retrieval(joint_fixture: JointFixture):  # noqa: F811
     """Test that configured values can be retrieved"""
-    url = "/values/a_string"
-    response = await joint_fixture.rest_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    assert json.loads(response.content) == {"a_string": "Some value"}
-
-
-@pytest.mark.asyncio
-async def test_happy_retrieval_complex_value(joint_fixture: JointFixture):  # noqa: F811
-    """Test that configured values can be retrieved"""
-    url = "/values/ghga_crypt4gh_public_key"
+    url = "/values/crypt4gh_public_key"
     response = await joint_fixture.rest_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     assert json.loads(response.content) == {
-        "ghga_crypt4gh_public_key": {
-            "version": 1.0,
-            "key": "30F32923F923IJF2RO23OIRJ2LK43JL3KJ4L",
-        }
+        "crypt4gh_public_key": "dWoWghAEVPcpHILEb5drJx59nF+of6YKuAOhKRpmegY="
     }
 
 
@@ -51,7 +39,7 @@ async def test_non_configured_value(joint_fixture: JointFixture):  # noqa: F811
     value_name = "not_configured"
     url = f"/values/{value_name}"
     response = await joint_fixture.rest_client.get(url)
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.status_code == status.HTTP_404_NOT_FOUND
     assert (
         json.loads(response.content)["detail"]
         == f"The value {value_name} is not configured"
@@ -61,6 +49,17 @@ async def test_non_configured_value(joint_fixture: JointFixture):  # noqa: F811
 @pytest.mark.asyncio
 async def test_retrieve_all_values(joint_fixture: JointFixture):  # noqa: F811
     """Test that the all-values endpoint works"""
-    response = await joint_fixture.rest_client.get("/values/")
+    response = await joint_fixture.rest_client.get("/values")
     assert response.status_code == status.HTTP_200_OK
-    assert json.loads(response.content) == joint_fixture.config.well_known_values
+    assert json.loads(response.content) == {
+        "crypt4gh_public_key": "dWoWghAEVPcpHILEb5drJx59nF+of6YKuAOhKRpmegY="
+    }
+
+
+@pytest.mark.asyncio
+async def test_config_filtering(joint_fixture: JointFixture):  # noqa: F811
+    """Make sure you can only query values configured in the WellKnownsConfig class"""
+    value_name = "service_name"
+    url = f"/values/{value_name}"
+    response = await joint_fixture.rest_client.get(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
