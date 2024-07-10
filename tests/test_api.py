d8c893cab032696d6c203d2f5727397e12c64ef2
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2024-07-10 12:02:31 +0000

    License headers

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-# Copyright 2021 - 2023 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
 # for the German Human Genome-Phenome Archive (GHGA)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -60,7 +60,7 @@ async def test_non_configured_value(
     assert (
         response.json()["detail"]
         == f"The value {
-        value_name} is not configured"
+            value_name} is not configured"
     )
 
 
