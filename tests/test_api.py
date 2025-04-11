04c535e3f3000e2c999a9ef3dd7963f433572a0f
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-04-11 11:52:20 +0200

    Update dependencies and template files (#13)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# Copyright 2021 - 2025 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
 # for the German Human Genome-Phenome Archive (GHGA)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -57,11 +57,7 @@ async def test_non_configured_value(
     url = f"/values/{value_name}"
     response = await joint_fixture.rest_client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert (
-        response.json()["detail"]
-        == f"The value {
-            value_name} is not configured"
-    )
+    assert response.json()["detail"] == f"The value {value_name} is not configured"
 
 
 @pytest.mark.asyncio
